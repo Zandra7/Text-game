d68de0b27aab5109adab5fd462852f8fd499c840
Sandra Eng <sandra.eng@live.no>
2023-02-18 19:11:30 +0100

    Changed language to english

--- main.py
+++ main.py
@@ -1,92 +1,92 @@
 import random
 
-fienderDrept = 0
-liv = 10
-fiendeLiv = 0
-møter = ['monster', 'monster', 'monster',
-         'kiste']  # 1/4 chance to find a chest
+EnemiesKilled = 0
+health = 10
+enemyHealth = 0
+encounters = ['monster', 'monster', 'monster',
+         'chest']  # 1/4 chance to find a chest
 
 
 def start():
     print("")
-    print("Du har", liv, "liv")
-    print("Fienden har", fiendeLiv, "liv")
+    print("You have", health, "health")
+    print("The enemy has", enemyHealth, "health")
     print("")
-    global angrip
-    angrip = input("Vil du angripe? ")
+    global attack
+    attack = input("Do you want to attack? ")
     print("")
 
 
 def tryAgain():  # Try again
-    global liv
-    liv = random.randint(5, 10)
+    global health
+    health = random.randint(5, 10)
     start()
 
 
 def notTryAgain():  # Not trying again
-    if fienderDrept > 1:
-        print("Du beseiret", fienderDrept, "fiender")
+    if EnemiesKilled > 1:
+        print("You killed", EnemiesKilled, "enemies")
     else:
-        print("Du beseiret bare", fienderDrept, "fiende")
-    global fortsett
-    fortsett = "nei"
+        print("You only killed", EnemiesKilled, "enemies")
+    global continiue
+    continiue = "no"
 
 
-liv = random.randint(5, 10)
+health = random.randint(5, 10)
 
-fortsett = "ja"
-while fortsett == "ja":
-    møte_type = random.choice(møter)
-    if møte_type == "monster":  # If you encounter a monster
-        fiendeLiv = random.randint(1, 10)
+continiue = "yes"
+while continiue == "yes":
+    encounter_type = random.choice(encounters)
+    if encounter_type == "monster":  # If you encounter a monster
+        enemyHealth = random.randint(1, 10)
         start()
-        if angrip == "ja":  # If you want to attack
-            if liv > fiendeLiv:  # If you have more health than the enemy
-                print("Du drepte fienden")
-                fienderDrept += 1
-                liv -= fiendeLiv
-            elif liv == fiendeLiv:  # If you have the same amount of health as the enemy
-                liktLiv = random.randint(1, 2)
-                if liktLiv == 1:  # If you beat the enemy
-                    print("Du drepte fienden")
-                    fienderDrept += 1
-                    liv -= fiendeLiv
+        if attack == "yes":  # If you want to attack
+            if health > enemyHealth:  # If you have more health than the enemy
+                print("You killed the enemy")
+                EnemiesKilled += 1
+                health -= enemyHealth
+            elif health == enemyHealth:  # If you have the same amount of health as the enemy
+                sameHealth = random.randint(1, 2)
+                if sameHealth == 1:  # If you beat the enemy
+                    print("You killed the enemy")
+                    EnemiesKilled += 1
+                    health -= enemyHealth
                 else:  # If you don't beat the enemy
-                    print("Du er død")
+                    print("You are dead")
                     print("")
-                    prøvIgjen = input("Vil du prøve igjen? ")
-                    if prøvIgjen == "ja":  # If you want to try again
+                    retry = input("Do you want to try again? ")
+                    if retry == "yes":  # If you want to try again
                         tryAgain()
                     else:  # If you don't want to try again
                         notTryAgain()
             else:  # If you have less health than the enemy
-                print("Du er død")
+                print("You are dead")
                 print("")
-                prøvIgjen = input("Vil du prøve igjen? ")
-                if prøvIgjen == "ja":  # If you want to try again
+                retry = input("Do you want to try again? ")
+                if retry == "yes":  # If you want to try again
                     tryAgain()
                 else:  # If you don't want to try again
                     notTryAgain()
-        elif angrip == "nei":  # If you don't want to attack
-            print("Du prøver å stikke av")
-            stikkAv = random.randint(1, 2)
-            if stikkAv == 1:  # If you get away
-                print("Du kom deg vekk")
+        elif attack == "no":  # If you don't want to attack
+            print("You try to run away")
+            runAway = random.randint(1, 2)
+            if runAway == 1:  # If you get away
+                print("You got away")
                 print("")
             else:  # If you don't get away
-                print("Du kom deg ikke vekk")
+                print("You could not get away")
                 print("")
-                prøvIgjen = input("Vil du prøve igjen? ")
-                if prøvIgjen == "ja":  # If you want to try again
+                retry = input("Do you want to try again? ")
+                if retry == "ja":  # If you want to try again
                     tryAgain()
                 else:  # If you don't want to try again
                     notTryAgain()
         else:  # If you write something else than "ja/nei"
-            print("Du er død")
-            fortsett = "nei"
-    elif møte_type == "kiste":  # If you find a chest
-        print("Du fant en kiste!")
-        liv += 3
-        print("Du fant 3 liv og du har nå", liv, "liv")
+            print("You are dead")
+            continiue = "no"
+    elif encounter_type == "chest":  # If you find a chest
+        print("You found a chest!")
+        health += 3
+        print("You found 3 lives and now have", health, "health")
         print("")
-        fortsett = input("Vil du fortsette? ")
+        continiue = input("Do you want to continiue? ")
