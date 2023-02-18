90949af1384dda8074fa2281a847cb6790c36aa0
Sandra Eng <sandra.eng@live.no>
2023-02-18 12:51:58 +0100

    Added comments and fixed health

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 fienderDrept = 0
 liv = 10
 fiendeLiv = 0
-møter = ['monster', 'monster', 'monster', 'kiste']
+møter = ['monster', 'monster', 'monster', 'monster', 'kiste'] # 1/5 sjanse for å finne en kiste
 
 def start():
     print("")
@@ -14,49 +14,52 @@ def start():
     angrip = input("Vil du angripe? ")
     print("")
 
+liv = random.randint(5, 10)
 
 fortsett = "ja"
 while fortsett == "ja":
     møte_type = random.choice(møter)
-    if møte_type == "monster":
-        liv = random.randint(5, 10)
+    if møte_type == "monster": # Hvis man møter et monster
         fiendeLiv = random.randint(1, 10)
         start()
 
-        if angrip == "ja":
-            if liv > fiendeLiv:
+        if angrip == "ja": # Hvis du vil angripe
+            if liv > fiendeLiv: # Hvis du har mer liv enn fienden
                 print("Du drepte fienden")
                 fienderDrept += 1
                 liv -= fiendeLiv
-            else:
+            else: # Hvis du har mindre liv enn fienden
                 print("Du er død")
                 print("")
                 prøvIgjen = input("Vil du prøve igjen? ")
-                if prøvIgjen == "ja":
+                if prøvIgjen == "ja": # Hvis du vil prøve igjen                
                     start()
-                else:
+                else: # Hvis du ikke vil prøve igjen
                     print("Du beseiret", fienderDrept, "fiende(r)")
                     fortsett = "nei"
-        elif angrip == "nei":
+        elif angrip == "nei": # Hvis du ikke vil angripe
             print("Du prøver å stikke av")
             stikkAv = random.randint(1, 2)
-            if stikkAv == 1:
+            if stikkAv == 1: # Hvis du kommer deg vekk
                 print("Du kom deg vekk")
-            else:
+                print("")
+            else: # Hvis du ikke kommer deg vekk
                 print("Du kom deg ikke vekk")
+                print("")
                 prøvIgjen = input("Vil du prøve igjen? ")
-                if prøvIgjen == "ja":
+                if prøvIgjen == "ja": # Hvis du vil prøve igjen
                     start()
-                else:
+                else: # Hvis du ikke vil prøve igjen
                     if fienderDrept > 1:
                         print("Du beseiret", fienderDrept, "fiender")
                     else:
                         print("Du beseiret bare", fienderDrept, "fiende")
                     fortsett = "nei"
-        else:
+        else: # Hvis du skriver noe annet enn ja/nei
             print("Du er død")
             fortsett = "nei"
-    elif møte_type == "kiste":
+    elif møte_type == "kiste": # Hvis man finner en kiste
         print("Du fant en kiste!")
-        print("Jippi!")
+        liv += 3
+        print("Du fant 3 liv og du har nå", liv, "liv")
         fortsett = input("Vil du fortsette? ")
